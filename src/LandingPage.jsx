fd51efa6bf3e8b279aa65d94accbd8f960a7068e
Marcus <marcus.veres@gmail.com>
2023-06-12 18:41:44 -0400

    Adds a rudimentary, placeholder landing page!

---
+++ src/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full bg-teal-600">
+      It twerks!!
+    </div>
+  );
+};
+
+export default LandingPage;
