8a8b63effd057ae825d40eb5b1c45fc3c048d074
Marcus <marcus.veres@gmail.com>
2023-06-12 21:26:49 -0400

    Adds tools section to landing page

---
+++ src/components/ToolsIUse.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import bg from "../assets/island-translucent.webp";
+
+const ToolsIUse = () => {
+  return (
+    <section
+      className="relative w-full pt-6 pb-4 bg-fixed bg-center bg-cover border-gray-600 md:pt-14 md:pb-12 xl:h-screen xl:flex xl:items-center xl:justify-center border-y"
+      style={{ backgroundImage: `url(${bg})` }} // directly use the imported image
+    >
+      <div className="container max-w-lg px-6 mx-auto lg:px-0 md:max-w-2xl xl:max-w-6xl xl:px-10">
+        <h2 className="text-4xl font-extrabold tracking-tight text-left border-b border-white md:pb-4 xl:text-6xl border-opacity-20">
+          Tools I Use
+        </h2>
+        .. template "tools". ..
+      </div>
+    </section>
+  );
+};
+
+export default ToolsIUse;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "../components/LandingHeader";
 import Things from "../components/ThingsIDo";
+import Tools from "../components/ToolsIUse";
 
 const LandingPage = () => {
   return (
     <div className="text-white bg-black bgx-gray-900">
       <Header></Header>
       <Things></Things>
+      <Tools></Tools>
     </div>
   );
 };
