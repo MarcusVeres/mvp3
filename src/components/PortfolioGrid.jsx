fbf9e16b253a99c99b5b99d3d553c710bb898033
Marcus <marcus.veres@gmail.com>
2023-06-13 21:24:00 -0400

    WIP :: Updates portfolio page with new images

---
+++ src/components/PortfolioGrid.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+
+import cardinal from "../assets/portfolio/cardinal.jpg";
+import fence from "../assets/portfolio/fence.jpg";
+import flowers from "../assets/portfolio/flowers.jpg";
+import glow from "../assets/portfolio/glow.jpg";
+import grandpa from "../assets/portfolio/grandpa.jpg";
+import knife from "../assets/portfolio/knife.jpg";
+import knob from "../assets/portfolio/knob.jpg";
+import lamp from "../assets/portfolio/lamp.jpg";
+import miki from "../assets/portfolio/miki.jpg";
+import pine from "../assets/portfolio/pine.jpg";
+import pipes from "../assets/portfolio/pipes.jpg";
+import shatters from "../assets/portfolio/shatters.jpg";
+import spade from "../assets/portfolio/spade.jpg";
+import streetlight from "../assets/portfolio/streetlight.jpg";
+import tunnel from "../assets/portfolio/tunnel.jpg";
+import trunk from "../assets/portfolio/trunk.jpg";
+import wall from "../assets/portfolio/wall.jpg";
+
+const PortfolioGrid = () => {
+  return (
+    <div className="container grid grid-cols-2 gap-2 p-2 mx-auto mt-12 text-white max-w-7xl sm:p-4 sm:gap-4 sm:grid-cols-3 lg:grid-cols-4">
+      <div className="flex items-center justify-center col-span-2 bg-black border border-white rounded-lg shadow-inner bg-opacity-40 border-opacity-10 sm:col-span-3 md:col-span-2">
+        <h1 className="p-0 m-0 text-6xl font-extrabold tracking-tighter text-transparent uppercase bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-500">
+          Portfolio
+        </h1>
+      </div>
+      <div className="md:col-start-3 card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${pine})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${cardinal})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${grandpa})` }}
+        ></div>
+      </div>
+      <div className="row-span-2 card-zoom">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${lamp})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${knob})` }}
+        ></div>
+      </div>
+      <div className="row-span-2 card-zoom">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${fence})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${trunk})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${pipes})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${flowers})` }}
+        ></div>
+      </div>
+      <div className="card-zoom md:col-span-2">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${knife})` }}
+        ></div>
+        <h3 className="hidden text-center uppercase text-md md:block md:text-2xl lg:text-3xl card-zoom-text xl:text-4xl">
+          Case Study I
+        </h3>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${tunnel})` }}
+        ></div>
+      </div>
+      <div className="row-span-2 card-zoom">
+        <div
+          className="card-zoom-image"
+          style={{
+            backgroundImage: `url(${streetlight})`,
+          }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${wall})` }}
+        ></div>
+      </div>
+      <div className="row-span-2 card-zoom">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${spade})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${miki})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${glow})` }}
+        ></div>
+      </div>
+      <div className="card-zoom aspect-golden">
+        <div
+          className="card-zoom-image"
+          style={{ backgroundImage: `url(${shatters})` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioGrid;
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Nav from "../components/Navigation";
+import Portfolio from "../components/PortfolioGrid";
 import Tools from "../components/ToolsIUse";
 import Footer from "../components/Footer";
 
@@ -9,6 +10,7 @@ const PorfolioPage = () => {
     <div className="text-white bg-black bgx-gray-900">
       <Nav></Nav>
       <Tools></Tools>
+      <Portfolio></Portfolio>
       <Footer></Footer>
     </div>
   );
